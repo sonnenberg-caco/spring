41d4daf8b918873c04a55c53e0b2feef9520c037
Ricardo da Cunha Sonnenberg <rsonnenberg@pmp.local>
2023-12-19 11:47:54 -0300

    Service Layer

--- src/main/java/com/example/demo4/resources/UserResource.java
+++ src/main/java/com/example/demo4/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.example.demo4.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo4.entities.User;
+import com.example.demo4.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User u = service.findById(id);
         return ResponseEntity.ok().body(u);
     }
 }
