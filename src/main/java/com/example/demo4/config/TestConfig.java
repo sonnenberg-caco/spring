b9fcac3c83644f21a42a09eb183ecf3a620d74e0
Ricardo Sonnenberg <sonnenberg.caco@gmail.com>
2024-03-08 11:55:04 -0300

    Payment

--- src/main/java/com/example/demo4/config/TestConfig.java
+++ src/main/java/com/example/demo4/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.demo4.entities.Category;
 import com.example.demo4.entities.Order;
 import com.example.demo4.entities.OrderItem;
+import com.example.demo4.entities.Payment;
 import com.example.demo4.entities.Product;
 import com.example.demo4.entities.User;
 import com.example.demo4.entities.enums.OrderStatus;
@@ -67,7 +68,7 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "33333333", "12345");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "33355555", "334455");
 
-        Order o1 = new Order(null, Instant.now(), OrderStatus.PAID, u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
@@ -80,5 +81,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
+
     }
+
 }
